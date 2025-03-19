380f79d9976d116bde4a12b1b933d0cf883d7306
hoornaert-cj <97850176+hoornaert-cj@users.noreply.github.com>
2025-03-19 09:41:43 -0700

    Second commit

---
+++ js/map.js
@@ -0,0 +1,8 @@
+// Initialize the map
+var map = L.map('map').setView([49.2827, -123.1207], 12);
+
+// Add tile layer
+L.tileLayer('https://tiles.stadiamaps.com/tiles/stamen_terrain/{z}/{x}/{y}{r}.png?api_key=22685591-9232-45c7-a495-cfdf0e81ab86', {
+    maxZoom: 18,
+    attribution: '&copy; <a href="https://stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://stamen.com/" target="_blank">Stamen Design</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a>',
+}).addTo(map);
